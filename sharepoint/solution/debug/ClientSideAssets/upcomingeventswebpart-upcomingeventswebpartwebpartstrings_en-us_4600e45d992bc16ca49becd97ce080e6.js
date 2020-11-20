026118292d2ab249b80351d5f64499237c967925
YeshaDoshiTatva <74341952+YeshaDoshiTatva@users.noreply.github.com>
2020-11-20 12:12:53 +0530

    Add files via upload

---
+++ sharepoint/solution/debug/ClientSideAssets/upcomingeventswebpart-upcomingeventswebpartwebpartstrings_en-us_4600e45d992bc16ca49becd97ce080e6.js
@@ -0,0 +1,11 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Description",
+    "ListURLFieldLabel": "List URL",
+    "DisplayItems": "Number of items to be displayed",
+    "NoItemFoundMessage": "No upcoming events are found",
+    "PropertiesMessage": "Please enter proper List URL and Number of items field",
+    "DisplayItemsMessage": "Please enter valid 'Number of items to be displayed' field",
+    "ListURLMessage": "Please enter valied 'List URL' field"
+  }
+});
